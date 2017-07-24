d9d0677d3ebd8d188df5478e73f110f8bad1255d
youngjuleenz <ylee447@aucklanduni.ac.nz>
2017-07-25 11:30:53 +1200

    Add project files and folders

---
+++ Tabs/Tabs/Tabs/CustomVision.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tabs
+{
+    public partial class CustomVision : ContentPage
+    {   
+        public CustomVision()
+        {
+            InitializeComponent();
+        }
+
+        private async void loadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+            file.Dispose();
+        }
+    }
+}
